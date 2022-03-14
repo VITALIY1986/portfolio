d2fd8b6d0a6d92ee94a94ed729d61422cbf52b12
VITALIY1986 <staslego1986@gmail.com>
2022-03-14 11:48:47 +0200

    asd

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Stack from '@/components/Stack';
 import { meta } from '@/data/config';
-import { previewImage } from './www.jpg';
+
 export default function Index() {
   return (
     <>
       <Head>
         <title>{meta.title}</title>
         <meta name="google" content="notranslate"></meta>
-        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:image" content='/static/favicon/apple-touch-icon.png' key="ogimage" />
       </Head>
       <div className="p-6 lg:p-20 max-w-4xl m-auto w-full">
         <Hero />
