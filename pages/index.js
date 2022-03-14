3d7ab5c8a2f205d0712aafbcb0c9bb2c016d5c0b
VITALIY1986 <staslego1986@gmail.com>
2022-03-14 11:43:16 +0200

    asd

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Stack from '@/components/Stack';
 import { meta } from '@/data/config';
-
+import { previewImage } from './www.jpg';
 export default function Index() {
   return (
     <>
       <Head>
         <title>{meta.title}</title>
         <meta name="google" content="notranslate"></meta>
-        <meta property="og:image" content='https://lux-texcv.com.ua/wp-content/uploads/2021/04/362.jpg' key="ogimage" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
       </Head>
       <div className="p-6 lg:p-20 max-w-4xl m-auto w-full">
         <Hero />
