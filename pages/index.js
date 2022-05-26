2872b05879bb911f7c512453aa62e68e6aad4398
VITALIY1986 <staslego1986@gmail.com>
2022-05-26 23:21:27 +0300

    Short Description of Changes Made

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import ProjectsDesigner from '@/components/ProjectsDesigner';
 import Stack from '@/components/Stack';
 import StackDesigner from '@/components/StackDesigner';
 import { meta } from '@/data/config';
-
+//qqqqq
 export default function Index() {
   return (
     <>
