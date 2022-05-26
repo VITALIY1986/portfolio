aed641ab4552aeae3ecf58fc509434a02d93f711
VITALIY1986 <staslego1986@gmail.com>
2022-05-27 00:34:02 +0300

    asd

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Contact from '@/components/Contact';
 import Head from 'next/head'
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
-import WhatsAppWidget from 'react-whatsapp-widget'
-import 'react-whatsapp-widget/dist/index.css'
+import WhatsAppWidget from "react-whatsapp-chat-widget";
+import "react-whatsapp-chat-widget/index.css";
 import ProjectsDesigner from '@/components/ProjectsDesigner';
 import Stack from '@/components/Stack';
 import StackDesigner from '@/components/StackDesigner';
@@ -28,7 +28,29 @@ export default function Index() {
           <ProjectsDesigner />
           <Contact />
           <WhatsAppWidget phoneNumber='+40732994556'
-          sendButton="Send"
+          position="right"
+          widgetWidth="300px"
+          widgetWidthMobile="260px"
+          autoOpen={true}
+          autoOpenTimer={2000}
+          messageBox={true}
+          messageBoxTxt="Hi Team, is there any related service available ?"
+          iconSize="80"
+          iconColor="white"
+          iconBgColor="tomato"
+          headerIcon="https://proficientdesigners.in/wp-content/themes/pd/img/logo-new.png"
+          headerIconColor="pink"
+          headerTxtColor="black"
+          headerBgColor="tomato"
+          headerTitle="Vitaliy Statkevick"
+          headerCaption="Online"
+          bodyBgColor="#bbb"
+          chatPersonName="Support"
+          chatMessage={<>Hi there 👋 <br /><br /> How can I help you?</>}
+          footerBgColor="#999"
+          btnBgColor="yellow"
+          btnTxtColor="black"
+          btnTxt="Start Chat"
          />
         </div>
       </div>
